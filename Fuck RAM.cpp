749dfbdbe082844a20369986f3bcde659e554d92
wzz12 <wzz12asan1212@outlook.com>
2026-08-27 09:59:18 +0800

    new memory leak

---
+++ Fuck RAM.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    while(true) {
+        char * p= (char *)malloc(100);
+        char * np = (char *)malloc(100);
+        p = np;
+        free(p);
+        free(np);
+    }
+    return 0;
+}
