398fc5adb04e59f628edaf188332c250707173a0
wzz12 <wzz12asan1212@outlook.com>
2026-08-28 08:43:40 +0800

    Enhance translation effectiveness

--- Fuck RAM.cpp
+++ Fuck RAM.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main() {
     while(true) {
-        char * p= (char *)malloc(1000);
-        char * np = (char *)malloc(1000);
+        char * p= (char *)malloc(2000);
+        char * np = (char *)malloc(2000);
         p = np;
     }
     return 0;
